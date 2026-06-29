81dd331ee99255f683f91eb586b001d3b4be8397
DNCGuanoluisa <dnc.guanoluisa@yavirac.edu.ec>
2026-06-29 00:51:58 -0500

    Versión lista para producción

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,23 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { AdminComponent } from './admin/admin.component';
 
-// Importa tus nuevos componentes
-import { CancionesComponent } from './admin/pages/canciones/canciones.component';
-import { CarpetasComponent } from './admin/pages/carpetas/carpetas.component';
-import { ArtistasComponent } from './admin/pages/artistas/artistas.component';
-import { AlbumesComponent } from './admin/pages/albumes/albumes.component';
-
 export const routes: Routes = [
-  {
-    path: 'admin',
-    component: AdminComponent,
-    children: [
-      { path: 'todas', component: CancionesComponent },
-      { path: 'carpetas', component: CarpetasComponent },
-      { path: 'artistas', component: ArtistasComponent },
-      { path: 'albumes', component: AlbumesComponent },
-      { path: '', redirectTo: 'todas', pathMatch: 'full' } // Ruta por defecto
-    ]
-  },
-  { path: '', redirectTo: '/admin/todas', pathMatch: 'full' }
+  // 1. La raíz del sitio ahora cargará correctamente el carrusel de inicio
+  { path: '', component: HomeComponent }, 
+  
+  // 2. El panel que estuvimos modificando solo se abrirá al escribir /admin
+  { path: 'admin', component: AdminComponent }, 
+  
+  // 3. Comodín: si escribes cualquier otra cosa, te regresa al inicio
+  { path: '**', redirectTo: '' } 
 ];
